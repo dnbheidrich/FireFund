e451e8b96ea2162287ba9965dbda50f1338a42d9
Dieter Heidrich <dnbheidrich@gmail.com>
2020-05-05 15:09:57 -0600

    changes

--- wwwsrc/src/views/RoomDetails.vue
+++ wwwsrc/src/views/RoomDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="component">
 
-<h1 class="text-white">Welcom to {{activeRoom.name}}</h1>
+<h1 class="text-white">Welcome to {{activeRoom.name}}</h1>
   </div>
 </template>
 
@@ -29,7 +29,7 @@ export default {
      async getRooms(){
       if(await this.$auth.isAuthenticated){
       this.$store.dispatch("getMyRooms");
-       if (!this.$store.state.rooms.length) {
+      if (!this.$store.state.rooms.length) {
       this.$store.dispatch("getRoomById", this.$route.params.roomId);
     } else {
       this.$store.dispatch(
