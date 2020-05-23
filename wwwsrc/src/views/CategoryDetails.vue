d1ce6e4f52e8ba48ba72b41cf2a626067691ed1f
Dieter Heidrich <dnbheidrich@gmail.com>
2020-05-22 18:48:51 -0600

    changes

--- wwwsrc/src/views/CategoryDetails.vue
+++ wwwsrc/src/views/CategoryDetails.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="CategoryDetails">
-<h1 class="text-white">Test</h1>
+<h1>Create Item</h1>
+<form @submit.prevent="">
+      <input
+        v-model="newItem.Name"
+        type="text"
+        name="make"
+        placeholder="Name..."
+        
+      />
+      <input
+       v-model="newItem.Description"
+        type="text"
+        name="model"
+        placeholder="Description..."
+      />
+      <input
+      v-model="newItem.ImgUrl"
+        type="text"
+        name="year"
+        placeholder="ImgUrl..."
+      />
+      <input
+      v-model="newItem.Quantity"
+        type="number"
+        name="year"
+        placeholder="Quantity..."
+      />
+      <input
+      v-model="newItem.Acv"
+        type="number"
+        name="year"
+        placeholder="Acv..."
+      />
+      <input
+      v-model="newItem.Rcv"
+        type="number"
+        name="year"
+        placeholder="Rcv..."
+      />
+       <button type="submit" class="btn btn-success">Submit</button>
+    </form>
 
   </div>
 </template>
@@ -10,7 +50,18 @@
 export default {
   name: 'CategoryDetails',
   data(){
-    return {}
+    return {
+      newItem: {
+        Name: "",
+        Description: "",
+        ImgUrl: "",
+        CategoryId: this.$route.params.categoryId,
+        Quantity: "",
+        Acv:"",
+        Rcv:"",
+
+      },
+    }
   },
   computed:{},
   methods:{},
